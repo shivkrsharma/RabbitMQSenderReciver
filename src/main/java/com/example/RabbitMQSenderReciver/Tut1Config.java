47abaacef5ce96bd338c337530ef75b04c033a23
shiv sharma <shivkrsharma@gmail.com>
2019-09-22 16:07:54 +0100

    working code with Reciver & sender

---
+++ src/main/java/com/example/RabbitMQSenderReciver/Tut1Config.java
@@ -0,0 +1,27 @@
+package com.example.RabbitMQSenderReciver;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class Tut1Config {
+
+    @Bean
+    public Queue hello() {
+        return new Queue("hello");
+    }
+
+    @Profile("receiver")
+    @Bean
+    public Tut1Receiver receiver() {
+        return new Tut1Receiver();
+    }
+
+    @Profile("sender")
+    @Bean
+    public Tut1Sender sender() {
+        return new Tut1Sender();
+    }
+}
